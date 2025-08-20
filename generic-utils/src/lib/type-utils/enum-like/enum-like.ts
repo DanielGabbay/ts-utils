7b790372606d9f3a1441118cf78183305aa7a49a
Daniel <daniel.g@sps.co.il>
2025-08-20 21:00:29 +0300

    fix: update EnumLike function to accept optional parameters and enhance documentation examples

--- generic-utils/src/lib/type-utils/enum-like/enum-like.ts
+++ generic-utils/src/lib/type-utils/enum-like/enum-like.ts
@@ -3,16 +3,22 @@
  *
  * @example
  * ```ts
- * const Colors = EnumLike('Red', 'Green', 'Blue'); //
+ * const Colors = EnumLike('Red', 'Green', 'Blue'); // { Red: 'Red', Green: 'Green', Blue: 'Blue' }
+ * ```
+ *
+ * @example with TypeFrom
+ * ```ts
+ * export const MyEnum = EnumLike('A', 'B', 'C'); // represents { A: 'A', B: 'B', C: 'C' } for value lookup and type safety
+ * export type MyEnum = TypeFrom<typeof MyEnum>; // represent the type that allows 'A' | 'B' | 'C'
  * ```
  */
 
 export function EnumLike<T extends readonly unknown[] | Record<PropertyKey, unknown>>(
-  input: T
+  input?: T
 ): {
   [K in T extends readonly (infer E)[] ? (E extends PropertyKey ? E : never) : keyof T]: K;
 };
-export function EnumLike<T extends object>(param: T | (keyof T)[]): { [K in keyof T]: K } {
+export function EnumLike<T extends object>(param?: T | (keyof T)[]): { [K in keyof T]: K } {
   if (!param || (Array.isArray(param) && param.length === 0)) {
     return {} as { [K in keyof T]: K };
   }
